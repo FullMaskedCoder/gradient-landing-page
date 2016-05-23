8fa1a29e24062ffffdb0a6bffc218e8c753c6be9
Breiden Busch <breiden.busch@gmail.com>
2016-05-22 21:27:48 -0600

    Fixed Nav

--- scripts.js
+++ scripts.js
@@ -1,10 +1,22 @@
+//----------------------------------------------------------------------------------
+// GLOBAL VARIABLES
+//----------------------------------------------------------------------------------
+var nav = document.getElementsByTagName('nav')[0];
+
+//resize event listener
+window.addEventListener('resize', widthCheck);
+
+function widthCheck(){
+  if (document.documentElement.clientWidth > 768) {
+      nav.style.height = '75px';
+  }
+}
+
 //Dropdown
 function dropdown() {
-    var nav = document.getElementsByTagName('nav')[0];
     if (nav.style.height != '200px') {
         nav.style.height = '200px';
     } else {
         nav.style.height = '75px';
     }
-    // nav.style.height = '200px';
 }
